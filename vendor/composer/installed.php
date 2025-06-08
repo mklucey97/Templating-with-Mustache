fbd5d64dfa0ff6f733dccde708c2329d880d3df6
mklucey97 <mklucey97@gmail.com>
2025-06-07 21:41:15 -0600

    add necessary vendor directoy to project to make sure the home and title page load properly

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lucey/templatingwithmustache',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '67508170876f1518e75cdf3bf1409529e500bbfe',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lucey/templatingwithmustache' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '67508170876f1518e75cdf3bf1409529e500bbfe',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mustache/mustache' => array(
+            'pretty_version' => 'v2.14.2',
+            'version' => '2.14.2.0',
+            'reference' => 'e62b7c3849d22ec55f3ec425507bf7968193a6cb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mustache/mustache',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
