032d4d0217050995a1831a203f8e76602d5a20ba
mklucey97 <mklucey97@gmail.com>
2025-06-07 21:13:33 -0600

    refactor: use Composer autoloader for Mustache instead of manual include

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
-
 // Author: Mary Lucey
 // Date: 6-7-25
 // Assignment 4: Templating with Mustache
 
-require_once 'mustache/mustache/src/Mustache/Autoloader.php';
-Mustache_Autoloader::register();
+require_once __DIR__ . '/vendor/autoload.php';
 
 $mustache = new Mustache_Engine;
 
@@ -20,5 +18,4 @@ $footer_data = ["year" => date("Y")];
 echo $mustache->render($header, $header_data) . PHP_EOL;
 echo $mustache->render($body, $body_data) . PHP_EOL;
 echo $mustache->render($footer, $footer_data) . PHP_EOL;
-
 ?>
