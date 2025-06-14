824226f17e066c88b4efc5992bfe608e1f253d5b
mklucey97 <mklucey97@gmail.com>
2025-06-14 10:11:17 -0600

    Initialize empty body data array for Mustache template.

--- title.php
+++ title.php
@@ -14,6 +14,7 @@ $body   = file_get_contents('templates/title.html');
 $footer = file_get_contents('templates/footer.html');
 
 $header_data = ["pagetitle" => "Title Generation Form"];
+$body_data   = [];
 
 
 
