8dc3bcce8e523170a38059b377032d79654b9f3a
mklucey97 <mklucey97@gmail.com>
2025-06-14 10:06:50 -0600

    Load footer template and complete initial Mustache template setup.

--- title.php
+++ title.php
@@ -7,8 +7,11 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 $mustache = new Mustache_Engine;
+
+
 $header = file_get_contents('templates/header.html');
 $body   = file_get_contents('templates/title.html');
+$footer = file_get_contents('templates/footer.html');
 
 
 
