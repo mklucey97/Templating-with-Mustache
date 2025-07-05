fc6a8d7f7781546659893afbf094c64d10bb2d15
mklucey97 <mklucey97@gmail.com>
2025-07-05 11:57:35 -0600

    Display error view with Mickey Mouse image and dynamic footer

--- process.php
+++ process.php
@@ -77,6 +77,15 @@ $mustache = new Mustache_Engine;
     echo $mustache->render(file_get_contents('templates/header.html'), [
         'pagetitle' => 'Error'
     ]);
+	echo $mustache->render(file_get_contents('templates/error.html'), [
+        'errorMessage' => "I'm sorry, your input was not valid. Please try again.",
+        'imageSrc' => 'images/mickeymouse.jpg',
+        'showTryAgain' => true
+    ]);
+    echo $mustache->render(file_get_contents('templates/footer.html'), [
+        'year' => date("Y")
+    ]);
+}
 
 
 
